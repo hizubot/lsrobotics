d775d4f131e8af419dccf239b0797a16f1ccc052
hizubot <chris11996@hotmail.com>
2025-03-26 23:27:33 -0500

    lv1 upgrade

--- script1.js
+++ script1.js
@@ -13,61 +13,107 @@ const slides = [
     },
     {   //Slide 3
         image: "srcs/img1/img1_3.png",
-        text: `To start coding, turn on your mBot2.<br>
-        Remember to turn it off at the end of the class.`,
+        text: `This is CyberPi, the main controller of mBot2. It has all the operation logic.`,
         className: ""
     },
     {   //Slide 4
         image: "srcs/img1/img1_4.png",
-        text: `<div>Open <a href='https://ide.mblock.cc' target='_blank' rel='noopener noreferrer'>mBlock IDE</a>, press <strong>Ctrl+Tab</strong> to return here.<br>
-        What is an IDE?</div>`,
+        text: `These are the sensors, they take data from the environment and send it to the controller.`,
         className: ""
     },
     {   //Slide 5
         image: "srcs/img1/img1_5.png",
-        text: `This is mBlock IDE.<br>
-        Let’s review the principal areas.`,
+        text: `These are the actuators, specifically the motors. They make the mBot move.`,
         className: ""
     },
     {   //Slide 6
         image: "srcs/img1/img1_6.png",
-        text: `In the Devices area, Delete CyberPi and Add mBot2.`,
+        text: `This is the mBot2 shield, it has a battery and many ports to connect sensors and actuators.`,
         className: ""
     },
     {   //Slide 7
         image: "srcs/img1/img1_7.png",
-        text: `Click Bluetooth, select your mBot2 and Pair.<br>
-        You should be in Live mode.`,
+        text: `This is the mBot2 chassis, it is made of aluminum. It gives a strong structure to the robot.`,
         className: ""
     },
     {   //Slide 8
         image: "srcs/img1/img1_8.png",
-        text: `<div>Open Setting and activate <strong>Block Area Fixed Pattern</strong>.<br>
-        Now you always see the available blocks.</div>`,
+        text: `These are the screws, their function is to join parts together. Look at their differences.`,
         className: ""
     },
     {   //Slide 9
         image: "srcs/img1/img1_9.png",
-        text: `Create your mBlock account!<br>
-        Click in Panda icon and select Google.`,
+        text: `These are the cables, they connect the actuators and sensors to the mBot2 shield.`,
         className: ""
     },
+
     {   //Slide 10
         image: "srcs/img1/img1_10.png",
-        text: `Select your Markham account and continue the process.<br>
-        You will see your profile icon.`,
+        text: `The tires, wheels and mini wheel allows the robot to move freely.`,
         className: ""
     },
     {   //Slide 11
         image: "srcs/img1/img1_11.png",
-        text: `<div>Click <strong>play hi until done</strong> block.<br>
-        What does mBot2 do?<div>`,
+        text: `To start coding, turn on your mBot2.<br>
+        Remember to turn it off at the end of the class.`,
         className: ""
     },
     {   //Slide 12
         image: "srcs/img1/img1_12.png",
-        text: `Change the name of your project and click Save.<br>
-        There are more options in File menu.`,
+        text: `<div><a href='https://ide.mblock.cc' target='_blank' rel='noopener noreferrer'>Click here</a> to open mBlock IDE, press <strong>Ctrl+Tab</strong> to return here.<br>
+        What is an <span color="blue" title="Integrated Development Environment">IDE</span>?</div>`,
+        className: ""
+    },
+    {   //Slide 13
+        image: "srcs/img1/img1_13.png",
+        text: `This is mBlock IDE.<br>
+        Let’s review the principal areas.`,
+        className: ""
+    },
+    {   //Slide 14
+        image: "srcs/img1/img1_14.png",
+        text: `In the Devices area, Delete CyberPi and Add mBot2.`,
+        className: ""
+    },
+    {   //Slide 15
+        image: "srcs/img1/img1_15.png",
+        text: `Set mBot2 as mostly used device. Next time mBot2 will appear as default device in mBlock.`,
+        className: ""
+    },
+    {   //Slide 16
+        image: "srcs/img1/img1_16.png",
+        text: `Click Bluetooth, select your mBot2 and Pair.<br>
+        You should be in Live mode.`,
+        className: ""
+    },
+    {   //Slide 17
+        image: "srcs/img1/img1_17.png",
+        text: `<div>Open Setting and activate <strong>Block Area Fixed Pattern</strong>.<br>
+        Now you always see the available blocks.</div>`,
+        className: ""
+    },
+    {   //Slide 18
+        image: "srcs/img1/img1_18.png",
+        text: `Create your mBlock account!<br>
+        Click in Panda icon and select Google.`,
+        className: ""
+    },
+    {   //Slide 19
+        image: "srcs/img1/img1_19.png",
+        text: `Select your Markham account and continue the process.<br>
+        You will see your profile icon.`,
+        className: ""
+    },
+    {   //Slide 20
+        image: "srcs/img1/img1_20.png",
+        text: `Remember to log in to all classes to save your projects to the cloud.<br>
+        Notice the change in the icon.`,
+        className: ""
+    },
+    {   //Slide 21
+        image: "srcs/img1/img1_21.png",
+        text: `<div>Click <strong>play hi until done</strong> block.<br>
+        What does mBot2 do?<div>`,
         className: ""
     },
 ];
--- script3.js
+++ script3.js
@@ -1,108 +1,131 @@
 const slides = [
     {   //Slide 1
-        image: "srcs/img2/img2_1.png",
-        text: `Learn how to drive the mBot2 with precision.`,
-        className: "anim-move-up"
+        video: "srcs/img3/img3_1.mp4",
+        text: `Learn how to control lights with mBot2.`,
+        className: ""
     },
     {   //Slide 2, 20x10 board
-        video: "srcs/img2/img2_2.mp4",
-        text: `What kind of movements do you know?<br>
-        Can you give more examples?`,
+        image: "srcs/img3/img3_2.png",
+        text: `What is light? Why is it important?<br>
+        Do you know some light sources?`,
         className: ""
     },
     {   //Slide 3
-        image: "srcs/img2/img2_3.png",
-        text: `What makes mBot2 move?`,
+        image: "srcs/img3/img3_3.png",
+        text: `How does mBot2 emit light?`,
         className: "anim-shake"
     },
     {   //Slide 4
-        image: "srcs/img2/img2_4.png",
-        text: `The mBot2 uses two encoder motors to move!<br>
-        Where else do you find motors?`,
+        image: "srcs/img3/img3_4.png",
+        text: `The mBot2 uses five LED to emit light!<br>
+        What is an LED? Where else do you find them?`,
         className: ""
     },
     {   //Slide 5
-        video: "srcs/img2/img2_5.mp4",
+        video: "srcs/img3/img3_5.mp4",
         text: `Let’s start coding!<br>
-        Create a program that moves forward the mBot2.`,
+        Create a program that plays an LED animation!`,
         className: ""
     },
     {   //Slide 6
-        image: "srcs/img2/img2_6.png",
+        image: "srcs/img3/img3_6.png",
         text: `Drag the following blocks to the workspace.<br>
         What happens when you press the Button A in mBot2?`,
         className: ""
     },
     {   //Slide 7, 12x18 white box
-        image: "srcs/img2/img2_7.png",
-        text: `Let’s modify the speed!<br>
-        What happens when you change RPM to 30?<br>
-        What does RPM mean?`,
+        image: "srcs/img3/img3_7.png",
+        text: `Try all the other animations.<br>
+        Which is your favorite?`,
         className: ""
     },
     {   //Slide 8
-        video: "srcs/img2/img2_8.mp4",
-        text: `At same time, more speed is more distance!`,
+        image: "srcs/img3/img3_8.png",
+        text: `Change the second block to the display block.<br>
+        Press the Button A in mBot2 and see what happens.`,
         className: ""
     },
     {   //Slide 9
-        image: "srcs/img2/img2_9.png",
-        text: `Let’s modify the time!<br>
-        What happens when you change secs to 2?`,
+        image: "srcs/img3/img3_9.png",
+        text: `When you click in one color, an individual led editor will display.<br>
+        Change the colors and try your code!`,
         className: ""
     },
     {   //Slide 10
-        video: "srcs/img2/img2_10.mp4",
-        text: `At same speed, more time is more distance!`,
+        image: "srcs/img3/img3_10.png",
+        text: `Your mBot2 is living in Peru, so let’s display the Peruvian flag!`,
         className: ""
     },
     {   //Slide 11
-        video: "srcs/img2/img2_11.mp4",
-        text: `Can mBot2 return to the starting point?`,
+        image: "srcs/img3/img3_11.png",
+        text: `This code will display the Peruvian flag!<br>
+        Let’s try the Mexican flag, do you remember the colors?`,
         className: ""
     },
     {   //Slide 12
-        image: "srcs/img2/img2_12.png",
-        text: `Add a new block and change to moves backward.<br>
-        Use the same speed and time.`,
+        image: "srcs/img3/img3_12.png",
+        text: `This code will display the Mexican flag!<br>
+        Which flag is it like? Let’s try the French flag, do you remember the colors?`,
         className: ""
     },
     {   //Slide 13
-        image: "srcs/img2/img2_1.png",
-        text: `Create a program that makes mBot2 turn right.`,
-        className: "anim-rotate"
+        image: "srcs/img2/img2_13.png",
+        text: `This code will display the French flag!<br>
+        Finally, let’s try the German flag, do you remember the colors?`,
+        className: ""
     },
     {   //Slide 14
-        image: "srcs/img2/img2_14.png",
-        text: `Drag the following blocks to the workspace.<br>
-        What happens when you press the Button B in mBot2?`,
+        image: "srcs/img3/img3_14.png",
+        text: `This code will display the German flag!<br>
+        How does black look like?`,
         className: ""
     },
     {   //Slide 15
-        video: "srcs/img2/img2_15.mp4",
-        text: `One robot was programmed to turn right and the other turn left.<br>
-        Which mBot2 moves clockwise?`,
+        video: "srcs/img3/img3_15.mp4",
+        text: `Let’s code a sequence of colors!<br>
+        We will program a traffic light.`,
         className: ""
     },
     {   //Slide 16
-        image: "srcs/img2/img2_16.png",
-        text: `Change RPM and secs to make mBot2 turn right 90 degrees.`,
+        image: "srcs/img3/img3_16.png",
+        text: `Drag the following blocks to the workspace.<br>
+        This code will display red color for 1 second, then turn of all LED.`,
         className: ""
     },
     {   //Slide 17
-        image: "srcs/img2/img2_17.png",
-        text: `<div><strong>Challenge:</strong> Move mBot2 in the following shape.</div>`,
+        image: "srcs/img3/img3_17.png",
+        text: `Add two more blocks. Change the time and color to match a traffic light!<br>
+        What happens after yellow?`,
         className: ""
     },
     {   //Slide 18
-        video: "srcs/img2/img2_18.mp4",
-        text: `Can mBot2 do more complex movements?`,
+        image: "srcs/img3/img3_18.png",
+        text: `<div>From Control blocks, drag the <strong>forever loop</strong> and wrap all the display blocks.<br>
+        What is a loop?<div>`,
         className: ""
     },
     {   //Slide 19
-        image: "srcs/img2/img2_19.png",
-        text: `Change the name of your project and click Save.<br>
-        There are more options in File menu.`,
+        image: "srcs/img3/img3_19.png",
+        text: `Drag the following blocks to the workspace. Press the space key in your keyboard.<br>
+        What is the difference?`,
+        className: ""
+    },
+    {   //Slide 20
+        image: "srcs/img3/img3_20.png",
+        text: `Red light will not turn off. Add a delay and change the color to green.<br>
+        What is a delay?`,
+        className: ""
+    },
+    {   //Slide 21
+        image: "srcs/img3/img3_21.png",
+        text: `Complete the traffic light code with delays.<br>
+        The last one is important?`,
+        className: ""
+    },
+    {   //Slide 22
+        video: "srcs/img3/img3_22.mp4",
+        text: `<div><strong>Challenge:</strong> Red light, green light!<br>
+        mBot2 moves on green light and stops on red light.<div>`,
         className: ""
     },
 ];
